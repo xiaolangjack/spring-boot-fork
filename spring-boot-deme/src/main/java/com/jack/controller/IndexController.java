3c4f332b3c720a6565ce6dbfb3d928d49efecaac
Jacky <workinglang@163.com>
2023-10-10 13:45:38 +0800

    添加首页

--- spring-boot-deme/src/main/java/com/jack/controller/IndexController.java
+++ spring-boot-deme/src/main/java/com/jack/controller/IndexController.java
@@ -1,13 +1,7 @@
 package com.jack.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created By: Jacky<workinglang@163.com>
@@ -17,12 +11,25 @@ import java.util.Map;
 @RestController
 public class IndexController {
 	@GetMapping("/")
-	public Map<String, Object> index() {
-		HashMap<String, Object> data = new HashMap<>();
-		data.put("time", new Date());
-		data.put("data", Collections.EMPTY_MAP);
-		data.put("code", HttpStatus.OK.value());
-		data.put("msg", "success");
-		return data;
+	public String index() {
+		return "<!DOCTYPE html>\n" +
+				"<html lang=\"en\">\n" +
+				"<head>\n" +
+				"    <meta charset=\"UTF-8\">\n" +
+				"    <title>SpringBoot 源码阅读</title>\n" +
+				"    <style>\n" +
+				"        body{\n" +
+				"            background: #d4cddc;\n" +
+				"            color: #2b686a\n" +
+				"        }\n" +
+				"    </style>\n" +
+				"</head>\n" +
+				"<body>\n" +
+				"<div style=\"text-align: center;\">\n" +
+				"    <h1 style=\"width: 100%;\"> SpringBoot 源码阅读</h1>\n" +
+				"    <br><h4 style=\"width: 100%;\">Jacky Qiu&lt;workinglang@163.com&gt;</h4>\n" +
+				"</div>\n" +
+				"</body>\n" +
+				"</html>";
 	}
 }
